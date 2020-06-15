48119e178ec5ddf929869d04ac487d58a44ca7c8
Nate Sales <nate@nate.to>
2020-06-14 22:12:44 -0700

    Improve UX with usage message and invalid prefix check

--- crpki.py
+++ crpki.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+import sys
+import shutil
 import requests
 
 
@@ -10,7 +13,11 @@ class Color:
     END = '\033[0m'
 
 
-prefix = "2a09:11c0:130::/44"
+if len(sys.argv) != 2:
+    print(Color.RED + "[!]" + Color.END + " Usage: crpki [PREFIX]")
+    exit(1)
+
+prefix = sys.argv[1]
 
 query = """
         query {
@@ -34,15 +41,22 @@ print("Running query for " + prefix, end="\r")
 for i in range(0, 5):
     response = requests.post("https://rpki.cloudflare.com/api/graphql", json={"query": query})
     if response.status_code == 200:
-        print(Color.BOLD + prefix + Color.END + (" " * len(prefix)))
+        print(Color.BOLD + prefix + Color.END + (" " * (shutil.get_terminal_size().columns - len(prefix))))
+        if response.json()["data"]["bgp"] is None:
+            print(Color.RED + "[!]" + Color.END + " Prefix " + prefix + " doesn't look like a valid prefix.")
+            exit(1)
+
         for origin in response.json()["data"]["bgp"]:
             state = origin["validation"]["state"]
             if state == "Valid":
                 state = Color.GREEN + "Valid" + Color.END
             elif state == "Invalid":
                 state = Color.RED + "Invalid" + Color.END
+
+            print("  AS" + str(origin["asn"]) + " " + state)
+
             for roa in origin["validation"]["covering"]:
-                print("  " + state + " for AS" + str(origin["asn"]) + " " + roa["prefix"]["prefix"] + " max length " + str(roa["prefix"]["maxLength"]))
+                print("    - " + roa["prefix"]["prefix"] + " max length " + str(roa["prefix"]["maxLength"]))
 
         exit(0)
 print(Color.ERROR, "Maximum tries exceeded, last HTTP status code " + str(response.status_code))
